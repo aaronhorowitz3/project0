9048ebe49419247a652238dcf00f5b4c02e89184
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-17 10:28:37 -0700

    some html, css, smooth scrolling jQ

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+console.log('sanity check!!!!')
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 650);
+    return false;
+});
