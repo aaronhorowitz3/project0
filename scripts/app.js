3ec7c3e6c31b992df5f502280fae749ed1ea6a16
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-18 14:53:16 -0700

    restructured, changed out images, added heroooo

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
-console.log('sanity check!!!!')
+console.log('sanity check! you\'re totally not insane... ')
 
+// smooth scroll from nav to different sections
 $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+        scrollTop: $( $(this).attr('href') ).offset().top - 85
     }, 650);
     return false;
 });
