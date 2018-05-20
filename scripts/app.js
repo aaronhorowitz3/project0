64e0e3fab8c942ddea99d5ff1b4c02de009c8f2b
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-20 15:53:26 -0700

    styled contact form, made content tweaks, added scroll counter for nav functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,24 @@
-console.log('sanity check! you\'re totally not insane... ')
 
 // smooth scroll from nav to different sections
 $('#stickyNav a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top - 85
-    }, 650);
+    }, 1000);
     return false;
 });
 
-// carousel initiation
+$('.sidenav li a').click(function(){
+  $('html, body').animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 1000);
+  return false;
+})
+
+// carousel, sidenav initiation
 $(document).ready(function(){
+  $('.sidenav').sidenav();
   $('.materialboxed').materialbox();
-    var carousel_interval = 3000;
+    var carousel_interval = 4500;
     $('.carousel').carousel();
     var int;
     function run(){
@@ -33,3 +40,32 @@ $(document).ready(function(){
     fullWidth: true,
     indicators: true,
   });
+
+// calculating distance scrolled
+$(window).on('scroll', function(){
+  var distanceScrolled = $(window).scrollTop();
+  console.log(distanceScrolled);
+  if (distanceScrolled > 638) {
+    $('#stickyNavAbout').addClass('scrolledTo');
+  } else {
+    $('#stickyNavAbout').removeClass('scrolledTo');
+  };
+  if (distanceScrolled > 1050) {
+    $('#stickyNavAbout').removeClass('scrolledTo');
+    $('#stickyNavProjects').addClass('scrolledTo');
+  } else {
+    $('#stickyNavProjects').removeClass('scrolledTo');
+  };
+  if (distanceScrolled > 1541) {
+    $('#stickyNavProjects').removeClass('scrolledTo');
+    $('#stickyNavTestimonials').addClass('scrolledTo');
+  } else {
+    $('#stickyNavTestimonials').removeClass('scrolledTo');
+  };
+  if (distanceScrolled > 1642) {
+    $('#stickyNavTestimonials').removeClass('scrolledTo');
+    $('#stickyNavContact').addClass('scrolledTo');
+  } else {
+    $('#stickyNavContact').removeClass('scrolledTo');
+  };
+})
