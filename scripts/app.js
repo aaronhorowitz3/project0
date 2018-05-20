e7a6f1dd42094fce4259e0d67f9f287b9683f144
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-20 16:28:28 -0700

    tweaked classes of nav anchors

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
 
-// smooth scroll from nav to different sections
+// smooth scroll from navs to different sections
+// top nav:
 $('#stickyNav a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 85
-    }, 1000);
-    return false;
+  $('html, body').animate({
+    scrollTop: $( $(this).attr('href') ).offset().top - 85
+  }, 1000);
+  return false;
 });
 
+// responsive side nav
 $('.sidenav li a').click(function(){
   $('html, body').animate({
     scrollTop: $( $(this).attr('href') ).offset().top
@@ -18,33 +20,35 @@ $('.sidenav li a').click(function(){
 $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.materialboxed').materialbox();
-    var carousel_interval = 4500;
-    $('.carousel').carousel();
-    var int;
-    function run(){
-        int = setInterval(function()
-        {
-            $('.carousel').carousel('next');
-        }, carousel_interval);
-    }
+  var carousel_interval = 4500;
+  $('.carousel').carousel();
+  var int;
+  function run(){
+    int = setInterval(function()
+    {
+      $('.carousel').carousel('next');
+    }, carousel_interval);
+  }
 
-// carousel pause
-    function stop(){
+  // carousel pause
+  function stop(){
     clearInterval(int);
-    }
-    $('.carousel').hover(stop, run);
-    run();
-    });
+  }
+  $('.carousel').hover(stop, run);
+  run();
+});
 
-    $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true,
-  });
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+});
 
 // calculating distance scrolled
 $(window).on('scroll', function(){
   var distanceScrolled = $(window).scrollTop();
   console.log(distanceScrolled);
+
+// applying bottom border class to each respective nav anchor
   if (distanceScrolled > 638) {
     $('#stickyNavAbout').addClass('scrolledTo');
   } else {
@@ -68,4 +72,28 @@ $(window).on('scroll', function(){
   } else {
     $('#stickyNavContact').removeClass('scrolledTo');
   };
+})
+
+$('#stickyNavAbout').on('click', function(){
+  $('#stickyNavProjects').removeClass('scrolledTo');
+  $('#stickyNavTestimonials').removeClass('scrolledTo');
+  $('#stickyNavContact').removeClass('scrolledTo');
+})
+
+$('#stickyNavProjects').on('click', function(){
+  $('#stickyNavAbout').removeClass('scrolledTo');
+  $('#stickyNavTestimonials').removeClass('scrolledTo');
+  $('#stickyNavContact').removeClass('scrolledTo');
+})
+
+$('#stickyNavTestimonials').on('click', function(){
+  $('#stickyNavProjects').removeClass('scrolledTo');
+  $('#stickyNavAbout').removeClass('scrolledTo');
+  $('#stickyNavContact').removeClass('scrolledTo');
+})
+
+$('#stickyNavContact').on('click', function(){
+  $('#stickyNavProjects').removeClass('scrolledTo');
+  $('#stickyNavTestimonials').removeClass('scrolledTo');
+  $('#stickyNavAbout').removeClass('scrolledTo');
 })
