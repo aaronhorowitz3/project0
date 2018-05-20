e40ac7794d6478e92dad83b195aaa6e0457050b6
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-20 10:56:32 -0700

    got carousel with zoom and autoplay to work

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,42 @@
 console.log('sanity check! you\'re totally not insane... ')
 
 // smooth scroll from nav to different sections
-$('a').click(function(){
+$('#stickyNav a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top - 85
     }, 650);
     return false;
 });
+
+// carousel initialization
+// $(document).ready(function(){
+//    $('.carousel').carousel();
+
+// zoom add-on
+ // });
+
+
+ // carousel autoplay
+// $('.carousel').carousel();
+//  setInterval(function() {
+//    $('.carousel').carousel('next');
+//  }, 2000);
+//
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+    var carousel_interval = 1000;
+    $('.carousel').carousel();
+    var int;
+    function run(){
+        int = setInterval(function()
+        {
+            $('.carousel').carousel('next');
+        }, carousel_interval);
+    }
+    function stop(){
+    clearInterval(int);
+    }
+    $('.carousel').hover(stop, run);
+    run();
+    });
